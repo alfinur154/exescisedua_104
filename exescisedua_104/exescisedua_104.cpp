3348fdc386952a966300c7b3571237cff2254131
alfinur154 <alfinur27@gmail.com>
2023-06-05 10:48:20 +0700

    exercise

--- exescisedua_104/exescisedua_104.cpp
+++ exescisedua_104/exescisedua_104.cpp
@@ -1,20 +1,86 @@
-// exescisedua_104.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Penerbit; 
+class Pengarang {
+public:
+	string nama;
+	vector<Penerbit*> daftar_Penerbit;
+	void penerbit(string pNama);
+	~Pengarang() {
+		cout << "Pengarang \"" << nama << "\" tidak ada\n";
+	}
+	void tambahPenerbit(Penerbit*);
+	void cetakpenerbit();
+
+};
+
+class Pengarang; {
+public:
+	string nama;
+	vector<Pengarang*> daftar_Pengarang;
+
+	Pengarang(string pNama) :nama(pNama) {
+		cout << "pengarang \"" << nama << "\" ada\n";
+	}
+	~engarang() {
+		cout << "pengarang \"" << nama << "\" tidak ada\n";
+	}
+
+	void  tambahPengarang(pengarang*);
+	void cetakPengarang();
+
+};
+
+class buku; {
+public:
+	string nama;
+	vector<pengarang*> nama_buku;
+
+	buku(string pNama) :nama(pNama) {
+		cout << "buku \"" << nama << "\" tidak ada\n";
+	}
+	~buku() {
+		cout << "buku \"" << nama << "\" tidak ada\";
+	}
+
+	void cetakbuku();
+
+};
+
+void pengarang::tambahPenerbit(penerbit* pPenerbit) {
+	daftar_penerbit.push_back(pPenerbit);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void pengarang::cetakpenerbit() {
+	cout << "pengarang  mencetak penerbit \"" << this->nama << "\":\n";
+	for (auto& a : daftar_penerbit) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void penerbit::tambahPengarang(pengarang* pPengarang) {
+	daftar_pengarang.push_back(pPengarang);
+	pPengarang->tambahPenerbit(this);
+}
+
+void penerbit::cetakPengarang() {
+	cout << "Daftar Pengarang yang mencetak penerbit\"" << this->nama << "\":\n";
+	for (auto& a : daftar_Pengarang) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+
+void penerbit::cetakbuku() {
+	cout << "Daftar Buku yang dicetak penerbit\"" << this->nama << "\":\n";
+	for (auto& a : daftar_penerbit) {
+		cout << a : a->nama << "\n";
+	}
+	cout << endl;
+
+int main() {
+	pengarang* varPengarang1 = new pengarang("irfan")
+
+}
